8a4d689e0da7d42dabbbd04db7b5de8ad647a3c8
byecu <166774348+byecu@users.noreply.github.com>
2024-09-26 10:54:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -119,50 +119,5 @@ conf_threshold = float(st.sidebar.slider("Select the Confidence Threshold", 10, 
 input_file = None
 temporary_location = None
 
-# Image Processing Section
-if source_radio == "IMAGE":
-    
-    st.sidebar.header("Upload")
-    input = st.sidebar.file_uploader("Choose an image.", type=("jpg", "png"))
-
-    if input is not None:
-        
-            uploaded_image = PIL.Image.open(input)
-            uploaded_image_cv = cv2.cvtColor(numpy.array(uploaded_image), cv2.COLOR_RGB2BGR)
-            visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold = conf_threshold)
-            st.image(visualized_image, channels = "BGR")
-        
-    else:
-    
-            st.write("Click on 'Browse Files' in the sidebar to run inference on an image.")
-
-input = None
-
-temporary_location = None
-
-if source_radio == "VIDEO":
-    st.sidebar.header("Upload")
-    input = st.sidebar.file_uploader("Choose an video.", type=("mp4"))
-
-    if input is not None:
-        
-        g = io.BytesIO(input.read())
-        temporary_location = "upload.mp4"
-
-        with open(temporary_location, "wb") as out:
-            out.write(g.read())
-
-        out.close()
-
-    if temporary_location is not None:
-        
-        play_video(temporary_location)
-        if st.button("Replay", type="primary"):
-            pass
-
-    else:
-        
-        st.write("Click on 'Browse Files' in the sidebar to run inference on an video.")
-
 if source_radio == "WEBCAM":
     play_live_camera()
