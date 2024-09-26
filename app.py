29298bb05e7ae7a6421208d1a46932b2daaebb5b
byecu <166774348+byecu@users.noreply.github.com>
2024-09-26 11:31:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from camera_input_live import camera_input_live
 def play_live_camera():
     image = camera_input_live()
     uploaded_image = PIL.Image.open(image)
-    uploaded_image_cv = cv2.cvtColor(numpy.array(uploaded_image), cv2.COLOR_RGB2BGR)
     visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold)
     st.image(visualized_image, channels = "BGR")
     
