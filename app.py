b804bc2edb124201d94b6ec39ae1a11560f47bda
byecu <166774348+byecu@users.noreply.github.com>
2024-09-26 13:13:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ import openvino as ov
 import notebook_utils as notebook_utils
 import tarfile
 import subprocess
+import openvino.tools.mo as mo
+
 
 # OpenVINO Object Detection Model
 def download_and_convert_model():
