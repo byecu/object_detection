81e0bdb3c901f411cbd9f76aca3f0b16580edc82
byecu <166774348+byecu@users.noreply.github.com>
2024-09-26 11:35:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -100,11 +100,10 @@ def run_object_detection(video_source, conf_threshold):
     camera.release()
 
 # Live Webcam
-
 def play_live_camera():
     image = camera_input_live()
     uploaded_image = PIL.Image.open(image)
-    uploaded_image_cv = cv2.cvtColor(numpy.array(uploaded_image), cv2.COLOR_RGB2BGR)
+    uploaded_image_cv = cv2.cvtColor(np.array(uploaded_image), cv2.COLOR_RGB2BGR)
     visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold)
     st.image(visualized_image, channels = "BGR")
 
