5b05a680192c54c8695519b9425d9cf70c26afc9
byecu <166774348+byecu@users.noreply.github.com>
2024-09-26 11:49:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,6 @@ def play_live_camera():
     image = camera_input_live()
     uploaded_image = PIL.Image.open(image)
     uploaded_image_cv = cv2.cvtColor(np.array(uploaded_image), cv2.COLOR_RGB2BGR)
-    visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold)
     st.image(visualized_image, channels = "BGR")
 
 # Streamlit Interface
